315578cb476bd816394885fc556ab8b741546f6f
Developer322 <realblackstarmf@gmail.com>
2020-01-04 23:22:35 +0500

    part7a completed

--- redux-anecdotes/src/services/anecdotes.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const baseUrl = 'http://localhost:3001/anecdotes'
-
-export const getAllAnecdotes = async () => (await axios.get(baseUrl)).data
-
-export const createNewAnecdote = async anecdote => (await axios.post(baseUrl, anecdote)).data
-
-export const changeVotes = async (anecdoteId, votes) => (await axios.patch(`${baseUrl}/${anecdoteId}`, { votes: votes })).data
